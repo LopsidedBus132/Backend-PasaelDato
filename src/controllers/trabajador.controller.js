3f2da7a4e9a3fc81ec326804d212ca8e2de08b22
Javier Barraza <javier.barraza.d@mail.pucv.cl>
2025-05-26 15:14:24 -0400

    Se agrega servicio para obtener trabajador por id
    Tambien se ajusta llamada de getTrabajadores.

--- src/controllers/trabajador.controller.js
+++ src/controllers/trabajador.controller.js
@@ -9,14 +9,43 @@ const TrabajadorModel = require('../models/trabajador.model');
 const getTrabajadores = async (req, res) => {
   try {
     const usuarios = await TrabajadorModel.obtenerTrabajadores();
-    res.json(usuarios);
+    res.json({success: true, message: 'Trabajadores obtenidos correctamente.', objeto: usuarios});
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error del servidor' });
+    res.status(500).json({success: false, message: 'Ha ocurrido un error interno'});
   }  };
 
 
+  /**
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
+  const getTrabajadorByUid = async (req, res) => {
+    try {
+      const uid = req.params.uid;
+      const trabajador = await TrabajadorModel.obtenerTrabajadorByUid(uid);
+
+      if (!trabajador) {
+        return res.status(404).json({
+          success: false,
+          message: 'Trabajador no encontrado'
+        });
+      }
+
+      res.json({success: true, message: 'Trabajadores obtenidos correctamente.', objeto: trabajador});
+    } catch (error) {
+      console.error('Error al buscar trabajador:', error);
+
+      return res.status(500).json({
+        success: false,
+        message: 'Ha ocurrido un error interno'
+      });
+    }  
+  };
+  
+
 
 module.exports = {
-    getTrabajadores
+    getTrabajadores,
+    getTrabajadorByUid
 }
